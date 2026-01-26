8b8a2b48fcb708e80959af6d8bdb7303e5a633bc
Ujhyi <david.ujh@gmail.com>
2026-01-26 21:59:58 +0000

    register page added

---
+++ src/pages/auth/Register.tsx
@@ -0,0 +1,10 @@
+const RegisterPage: React.FC = () => {
+
+    return (
+        <div className="min-h-screen bg-gray-100 p-6">
+            <h1 className="text-3xl font-bold mb-6">Register Page</h1>
+        </div>
+    );
+};
+
+export default RegisterPage;
