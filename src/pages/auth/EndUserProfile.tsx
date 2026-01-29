02fa22cbebdf31dd4c03fe43dd5a89aa3130c638
Ujhyi <david.ujh@gmail.com>
2026-01-29 21:55:01 +0000

    TODO

---
+++ src/pages/auth/EndUserProfile.tsx
@@ -0,0 +1,13 @@
+// TODO: Complete your end_user profile
+
+
+const EndUserProfile: React.FC = () => {
+
+    return (
+        <div className="bg-gray-100 p-6 w-full">
+            <h1 className="text-3xl font-bold mb-6">TO DO - Complete your profile</h1>
+        </div>
+    );
+};
+
+export default EndUserProfile;
---
+++ src/pages/auth/ResetPasswordReq.tsx
@@ -0,0 +1,13 @@
+// TODO: Reset password request by email
+
+
+const ResetPasswordReq: React.FC = () => {
+
+    return (
+        <div className="bg-gray-100 p-6 w-full">
+            <h1 className="text-3xl font-bold mb-6">TO DO - ResetRequestPassword</h1>
+        </div>
+    );
+};
+
+export default ResetPasswordReq;
